7a6e038436d12f95a86be1396dda9f1f1d6433c2
d-daemon <10750312+d-daemon@users.noreply.github.com>
2024-06-23 16:38:50 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from discord.ext import commands
+import json
+
+# Load config
+with open('data/config.json') as f:
+    config = json.load(f)
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+PREFIX = config['prefix']
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+# Load cogs
+initial_extensions = [
+    'cogs.admin',
+    'cogs.fun',
+    'cogs.info',
+    'cogs.moderation',
+    'cogs.welcome'
+]
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+bot.run(TOKEN)
